0fdea691459a3b76ba2af2b49d8fe7d08eb99c9c
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-04-30 04:14:25 +0300

    updated

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const { json } = require('express')
 app.use(cors())
 app.use(express.json())
 app.use(cmd)
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 const port = process.env.PORT || 5000
 app.listen(port,() => console.log(`server run at http://localhost:${port}`))
