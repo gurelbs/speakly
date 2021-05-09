40cd5b57e7c90065fd8243e602fe13ea24485288
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-05-09 12:53:30 +0300

    updated

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,9 @@ let url;
 
 if (process.env.NODE_ENV === 'development'){
     url = 'http://localhost:5000/api'
-} else url = '/api'
+} else {
+    url = 'https://speakly.cf/api'
+}
 
 
 const api = axios.create({
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cmd = require('./router/cmd')
 const app = express()
