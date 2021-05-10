8961ef9cf0bf9ea3ce887efb925a2c97a473b342
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-05-10 04:08:59 +0300

    updated

--- router/cmd.js
+++ router/cmd.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const googleAnswer = require('./../utils/googleAnswer')
 const router = new express.Router()
-// const axios = require('axios')
-// const { default: api } = require('../client/src/api/api')
 
 router.post('/api/cmd', async (req, res) => {   
     const data = req.body
@@ -16,15 +14,4 @@ router.post('/api/cmd', async (req, res) => {
     }
 })
 
-// router.post('/api/voice', async (req,res) => {
-//     const data = req.body
-//     const {txt,lang} = data
-//     let apiURL = `http://api.voicerss.org/?key=2a0ec72724104343b35809b65a8634f8&hl=he-il&c=MP3&f=16khz_16bit_stereo&src=שלום`;
-//     try {
-//         const audioBuffer = await api.get(apiURL)
-//         res.send(audioBuffer).end()
-//     } catch (error) {
-//         res.send(error).end()
-//     }
-// })
 module.exports = router
