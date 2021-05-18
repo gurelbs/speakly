7c9c2f01c2e36798373aa0805fbb11cf725891c9
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-05-18 04:24:34 +0300

    updated

--- utils/googleAnswer.js
+++ utils/googleAnswer.js
@@ -26,66 +26,77 @@ const googleAnswer = async (term,lang) => {
             let finance = await page.$('g-card-section span')
             let calc = await page.$('#cwos')
             let weather = await page.$('#wob_wc')
+            let songLyrics = await page.$('.kp-blk')
+            let topNews = await page.$('g-scrolling-carousel g-inner-card')
+            
+            // [...document.querySelectorAll("g-scrolling-carousel g-inner-card")].map(s => s.innerText).join(' ').split('\n').join(' ')
+            // document.querySelector("#tsuid11").innerText.split('\n').join(', ')
             // document.querySelector("#kp-wp-tab-overview span").innerText
             // foundElement = await page.waitForSelector('.xpdopen .kp-header div, #kp-wp-tab-overview span, #knowledge-currency__updatable-data-column, #tw-container #tw-target-text, g-card-section span, #cwos, #wob_wc');
             // if (foundElement) {
-                if (answerBox){
-                    await navigationPromise;
-                    res = await page.evaluate(() => document.querySelector(".xpdopen .kp-header div").innerText
-                        .split('\n')
-                        .splice(0,2)
-                        .join(' ')
-                        .replace('ק״מ','קילומטר')
-                        .replace('קמ\"ר','קילומטר רבוע')
-                        .replace(/\([^)]*\)/g,'')
-                        .replace('/',': '))
-                }
-                if (wikiAnswer && !answerBox){
-                    await navigationPromise;
-                    res = await page.evaluate(() => document.querySelector("#kp-wp-tab-overview span").innerText)
-                }
-                if (currency){
-                    await navigationPromise;
-                    res = await page.evaluate(() => document
-                        .querySelector('#knowledge-currency__updatable-data-column > div').innerText
-                        .split('\n')
-                        .join(' ')
-                        .replace('שווה','שָׁוֶה')
-                        .replace('ביטקוין','בִּיטְקוֹיִיְן'))
-                }
-                if (translate){
-                    await navigationPromise;
-                    res = await page.evaluate(() => document.querySelector('#tw-container #tw-target-text').innerText)
-                }
-                if (finance){
-                    await navigationPromise;
-                    res = await page.evaluate(() => document.querySelector('g-card-section span').innerText)
-                }
-                if (calc){
-                    await navigationPromise;
-                    res = await page.evaluate(() => document.querySelector('#cwos').innerText)
-                }
-                if (weather){
-                    await navigationPromise;
-                    res = await page.evaluate(() => {
-                        let temp = document.querySelector("#wob_tm").innerText
-                        let loc = document.querySelector("#wob_loc").innerText
-                        let date = document.querySelector("#wob_dts").innerText
-                        let desc = document.querySelector('#wob_dc').innerText.replace('מעונן','מְעֻנָּן')
-                        return `מזג האוויר ב${loc}: ${temp}°, ${desc}. (${date}).`
-                    })
-                }
-            // } 
-            // else {
-            //     res = `קצת מביך... לא מצאתי מידע ישיר על ${term}`
-            // }
+            if (topNews) {
+                await navigationPromise;
+                res = await page.evaluate(() => [...document.querySelectorAll("g-scrolling-carousel g-inner-card")].map(s => s.innerText).join(', ').split('\n').join(' '))
+            }
+            if (songLyrics) {
+                await navigationPromise;
+                res = await page.evaluate(() => document.querySelector(".kp-blk").innerText.split('\n').join(', ').replace('הצגת עוד',''))
+            }
+            if (answerBox){
+                await navigationPromise;
+                res = await page.evaluate(() => document.querySelector(".xpdopen .kp-header div").innerText
+                    .split('\n')
+                    .splice(0,2)
+                    .join(' ')
+                    .replace('ק״מ','קילומטר')
+                    .replace('קמ\"ר','קילומטר רבוע')
+                    .replace(/\([^)]*\)/g,'')
+                    .replace('/',': '))
+            }
+            if (wikiAnswer && !answerBox){
+                await navigationPromise;
+                res = await page.evaluate(() => document.querySelector("#kp-wp-tab-overview span").innerText)
+            }
+            if (currency){
+                await navigationPromise;
+                res = await page.evaluate(() => document
+                    .querySelector('#knowledge-currency__updatable-data-column > div').innerText
+                    .split('\n')
+                    .join(' ')
+                    .replace('שווה','שָׁוֶה')
+                    .replace('ביטקוין','בִּיטְקוֹיִיְן'))
+            }
+            if (translate){
+                await navigationPromise;
+                res = await page.evaluate(() => document.querySelector('#tw-container #tw-target-text').innerText)
+            }
+            if (finance){
+                await navigationPromise;
+                res = await page.evaluate(() => document.querySelector('g-card-section span').innerText)
+            }
+            if (calc){
+                await navigationPromise;
+                res = await page.evaluate(() => document.querySelector('#cwos').innerText)
+            }
+            if (weather){
+                await navigationPromise;
+                res = await page.evaluate(() => {
+                    let temp = document.querySelector("#wob_tm").innerText
+                    let loc = document.querySelector("#wob_loc").innerText
+                    let date = document.querySelector("#wob_dts").innerText
+                    let desc = document.querySelector('#wob_dc').innerText.replace('מעונן','מְעֻנָּן')
+                    return `מזג האוויר ב${loc}: ${temp}°, ${desc}. (${date}).`
+                })
+            }
+            RENDER_CATCH.set(url, res)
+            await context.close(); 
+            return res || `לא הבנתי, אפשר לנסות שוב`
         } catch (e) {
+            RENDER_CATCH.set(url, res)
+            await context.close(); 
             console.log(e.message);
             return res || `לא הבנתי, אפשר לנסות שוב`
         }
-        RENDER_CATCH.set(url, res)
-        await context.close(); 
-        return res || `לא הבנתי, אפשר לנסות שוב`
 }
 
 module.exports = googleAnswer
