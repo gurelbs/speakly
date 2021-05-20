342c6f423051a37212291265804c4658f9078c97
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-05-20 04:38:57 +0300

    updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Document = require('./modules/Document')
 
 const io  = require("socket.io")(server,{
     cors: {
-        origin: "http://localhost:3000" || 'http://speakly.cf' || 'https://speakly.cf',
+        origin: "*",
         methods: ["GET", "POST"]
       }
 })
