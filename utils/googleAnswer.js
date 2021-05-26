a62ecfac2e9dffdc70e3d24271223beab55c0375
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-05-27 01:13:14 +0300

    updated

--- utils/googleAnswer.js
+++ utils/googleAnswer.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 const RENDER_CATCH = new Map()
+process.setMaxListeners(process.getMaxListeners() + 1);
 const googleAnswer = async (term,lang) => {
         const url = `https://google.com/search?q=${term}&hl=${lang}`
         if (RENDER_CATCH.has(url)) return RENDER_CATCH.get(url)
-        let res;
+        let res = '';
         let err = `לא מצאתי משהו רלוונטי על ${term}`;
         const browser = await puppeteer.launch({
             args: ['--no-sandbox'],
@@ -13,6 +14,7 @@ const googleAnswer = async (term,lang) => {
         const page = await context.newPage();
         await page.goto(url, {waitUntil:'domcontentloaded'});
         try {
+            res = '';
             let wikiAnswer = await page.$('#kp-wp-tab-overview');
             let currency = await page.$('#knowledge-currency__updatable-data-column');
             let translate = await page.$('#tw-container');
@@ -21,14 +23,20 @@ const googleAnswer = async (term,lang) => {
             let finance = await page.$('#knowledge-finance-wholepage__entity-summary');
             let speedAnswer = await page.$('#rso .xpdopen');
             let validateAnswer = !(currency || translate || calc || weather || finance)
+            let wholepage = await page.$('.kp-wholepage')
+            let innerCard = await page.$('g-inner-card')
+            let wikiExpand = await page.$('.kno-rdesc')
+            let lyrics = await page.$('.kp-header')
 
-            if (wikiAnswer && validateAnswer && !speedAnswer){
+            if (wikiAnswer && validateAnswer && !speedAnswer && !wholepage){
+                console.log('is wikiAnswer & validateAnswer & NOT speedAnswer & NOT wholepage');
                 await page.waitForSelector('#kp-wp-tab-overview',{visible: true})
-                return res = await page.evaluate(() => document.querySelector("#kp-wp-tab-overview span").innerText)
+                res += await page.evaluate(() => document.querySelector("#kp-wp-tab-overview").innerText)
             } 
             if (currency){
+                console.log('is currency');
                 await page.waitForSelector('#knowledge-currency__updatable-data-column',{visible: true,timeout:300})
-                return res = await page.evaluate(() => document
+                res += await page.evaluate(() => document
                 .querySelector('#knowledge-currency__updatable-data-column > div').innerText
                 .split('\n')
                 .join(' ')
@@ -36,16 +44,19 @@ const googleAnswer = async (term,lang) => {
                 .replace('ביטקוין','בִּיטְקוֹיְן'))
             }
             if (translate){
+                console.log('is translate');
                 await page.waitForSelector('#tw-container',{visible: true})
-                return res = await page.evaluate(() => document.querySelector('#tw-container #tw-target-text').innerText)
+                res += await page.evaluate(() => document.querySelector('#tw-container #tw-target-text').innerText)
             }
             if (calc){
+                console.log('is calc');
                 await page.waitForSelector('#cwos',{visible: true})
-                return res = await page.evaluate(() => document.querySelector('#cwos').innerText)
+                res += await page.evaluate(() => document.querySelector('#cwos').innerText)
             }
             if (weather){
+                console.log('is weather');
                 await page.waitForSelector('#wob_wc',{visible: true})
-                return res = await page.evaluate(() => {
+                return res += await page.evaluate(() => {
                     let temp = document.querySelector("#wob_tm").innerText
                     let loc = document.querySelector("#wob_loc").innerText
                     let date = document.querySelector("#wob_dts").innerText
@@ -55,37 +66,65 @@ const googleAnswer = async (term,lang) => {
                 
             }
             if (finance){
+                console.log('is finance');
                 await page.waitForSelector('#knowledge-finance-wholepage__entity-summary',{visible: true})
-                return res = await page.evaluate(() => document.querySelector('#knowledge-finance-wholepage__entity-summary span').innerText)
+                return res += await page.evaluate(() => document.querySelector('#knowledge-finance-wholepage__entity-summary span').innerText)
+            }
+            if (wikiExpand){
+                console.log('is wikiExpand');
+                await page.waitForSelector('.kno-rdesc span',{visible: true})
+                return res += await page.evaluate(() => document.querySelector('.kno-rdesc span').innerText)
+            }
+            if (wholepage && validateAnswer){
+                console.log('is wholepage');
+                return res += await page.evaluate(() => document.querySelector(".kp-wholepage").innerText
+                .split('\n')
+                .join(': ')
+                .trim()
+                .replace('\/',' - '))
             }
             if (speedAnswer && validateAnswer){
+                console.log('is speedAnswer & validateAnswer');
                 let isGoogleFirstAnswerResult = await page.evaluate(() => document.querySelector("#rso div span a").innerText === 'מידע על תקצירי תוצאות חיפוש ראשונות')
                 await page.waitForSelector('#rso .xpdopen',{visible: true})
-                console.log(isGoogleFirstAnswerResult)
+                console.log({'isGoogleFirstAnswerResult':isGoogleFirstAnswerResult})
                 if (isGoogleFirstAnswerResult){
-                    return res = await page.evaluate(() => document.querySelector("#rso .xpdopen").innerText
+                    res += await page.evaluate(() => document.querySelector("#rso .xpdopen").innerText
                     .split('\n')
-                    .slice(2,3)
+                    .slice(1,3)
                     .join(': ')
                     .trim()
                     .replace('\/',' - '))
                 } else {
-                    return res = await page.evaluate(() => document.querySelector("#rso .xpdopen").innerText
-                    .split('\n')
-                    .slice(0,2)
-                    .join(': ')
-                    .trim()
-                    .replace('\/',' - '))
+                    res += await page.evaluate(() => document.querySelector("#rso .xpdopen").innerText
+                        .split('\n')
+                        .join(': ')
+                        .trim()
+                        .replace('\/',' - '))
+                    }
                 }
+            if (innerCard && validateAnswer){
+                res += await page.evaluate(() => [...document.querySelectorAll("g-inner-card")].map(el => el.innerText).join(' '))
             }
             if (!speedAnswer && !validateAnswer && wikiAnswer){
-                res = 'יואו אשכרה לא מצאתי כלום רלוונטי'
+                res += await page.evaluate(() => document.querySelector(".g").innerText
+                    .split('\n')
+                    .join(' ')
+                    .trim())
+            }
+            if (lyrics){
+                console.log('is lyrics')
+                res += await page.evaluate(() => [...document.querySelectorAll("div")]
+                    .filter(el => el.getAttribute('data-attrid'))
+                    .map(el => el.innerText)
+                    .join(', '))
+                
             }
         } catch (e) {
             if (e instanceof puppeteer.errors.TimeoutError) {
-                return res = 'יש לי איזה באג משום מה'
+                return res += 'יש לי איזה באג משום מה'
             } else {
-                return  res = 'אין לי מושג מה הבעיה...'
+                return  res += 'אין לי מושג מה הבעיה אבל לא מצאתי מידע על '+ term;
             }
         }
         RENDER_CATCH.set(url, res)
