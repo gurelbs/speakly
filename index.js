6f277e999e5252fdc55e7cebaad65d5c4aa718e0
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-05-27 13:51:41 +0300

    updated

--- index.js
+++ index.js
@@ -32,21 +32,25 @@ if (process.env.NODE_ENV === 'production'){
 }
 let defaultData = ''
 io.on("connection", socket => {
-  console.log("New client connected");
-  socket.on('get-document', async documentId => {
-      const document = await findOrCreateDoc(documentId,defaultData)
-      socket.join(documentId)
-      socket.emit('load-document', document.data)
-      socket.on("send-chenges", delta => {
-          socket.broadcast.to(documentId).emit("receive-chenges",delta)
-      })
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
-      });
-      socket.on("save-document", async data => {
-          await Document.findByIdAndUpdate(documentId,{ data })
-      })
-  })
+    try {
+        console.log("New client connected");
+        socket.on('get-document', async documentId => {
+          const document = await findOrCreateDoc(documentId,defaultData)
+          socket.join(documentId)
+          socket.emit('load-document', document.data)
+          socket.on("send-chenges", delta => {
+              socket.broadcast.to(documentId).emit("receive-chenges",delta)
+          })
+          socket.on("disconnect", () => {
+            console.log("Client disconnected");
+          });
+          socket.on("save-document", async data => {
+              await Document.findByIdAndUpdate(documentId,{ data })
+          })
+        })
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
